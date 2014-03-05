a3b802f25d8ce5414b921570b59037dee54c1151
Maarten Versteegh <maartenversteegh@gmail.com>
2014-03-05 15:50:00 +0100

    added symbols to split on

---
+++ term-discovery-evaluation/symbols.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ------------------------------------
+# file: symbols.py
+# date: Wed March 05 15:19 2014
+# author:
+# Maarten Versteegh
+# github.com/mwv
+# maartenversteegh AT gmail DOT com
+#
+# Licensed under GPLv3
+# ------------------------------------
+"""symbols:
+
+"""
+
+
+SPLITS = """<EXCLUDE-name>
+<EXCLUDE>
+<exclude-Name>
+EXCLUDE
+IVER
+IVER-LAUGH
+LAUGH
+NOISE
+SIL
+UNKNOWN
+VOCNOISE""".split('\n')
+
+IGNORE = """{B_TRANS}
+{E_TRANS}""".split('\n')
